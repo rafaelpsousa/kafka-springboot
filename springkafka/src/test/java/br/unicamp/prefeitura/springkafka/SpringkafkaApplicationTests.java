114ca69bac3bd695a87894bbb31c01f98dbed554
rafael <rafael@ubuntu>
2022-04-24 15:09:13 -0300

    full project spring boot kafka

---
+++ springkafka/src/test/java/br/unicamp/prefeitura/springkafka/SpringkafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.unicamp.prefeitura.springkafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringkafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
